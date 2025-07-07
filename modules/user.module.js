32f6a5b745b35c3826a4ce016c7e8e1632d050de
HD Decodeup <bpa@decodeup.email>
2025-07-07 10:40:55 +0530

    feat: implement user signup and login routes with validation and JWT authentication

--- modules/user.module.js
+++ modules/user.module.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, "User name is required"] },
   email: { type: String, required: [true, "email is required"] },
+  password: { type: String, required: [true, "Password is required"] },
   role: {
     type: String,
     enum: ["waiter", "chef", "manager", "user"],
